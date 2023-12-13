f98df89ae7c60350447cd9eabc0692cc26a4fdbb
Sharath Boini <104951203+SharathB1@users.noreply.github.com>
2023-12-12 20:49:41 -0500

    Create tfdeploy.groovy

---
+++ Sample Terraform Stages/tfdeploy.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {git branch: 'main', credentialsId: '<YOUR_CREDENTIALS_ID>', url: '<YOUR_URL>'
+            }
+        }
+        stage('Terraform init') {
+            steps {
+                bat 'terraform init'
+            }
+        }
+        stage('Terraform apply') {
+            steps {
+                bat 'terraform apply --auto-approve'
+            }
+        }
+        stage('Terraform destroy') {
+            steps {
+                bat 'terraform destroy --auto-approve'
+            }
+        }
+        
+    }
+}
